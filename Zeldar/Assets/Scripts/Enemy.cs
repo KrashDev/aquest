43f115d45314a0a09783d62f8d50267b27619feb
dgabai1 <mrdgabai@gmail.com>
2020-08-07 22:24:52 -0400

    initial

---
+++ Zeldar/Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+}
